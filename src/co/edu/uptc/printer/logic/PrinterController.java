ed1bef9bce7d410a796bfaddb9ce5d8c465a701d
Nicolas <nicolas.garzon02@uptc.edu.co>
2024-02-04 19:30:04 -0500

    Elminated unnecesary method

--- src/co/edu/uptc/printer/logic/PrinterController.java
+++ src/co/edu/uptc/printer/logic/PrinterController.java
@@ -1,6 +1,9 @@
 package co.edu.uptc.printer.logic;
 
+import co.edu.uptc.printer.model.Archive;
 import co.edu.uptc.printer.model.Printer;
+
+import java.util.ArrayList;
 
 public class PrinterController {
 
@@ -39,6 +42,30 @@ public class PrinterController {
     public String showLowSheet(){
 
         return "";
+    }
+
+    public int totalSheet(ArrayList<Archive> documents){
+        int total = 0;
+        for (Archive document : documents) {
+            total = total + document.getPages();
+        }
+        return total;
+    }
+
+    public boolean checkSheets(int requestedSheets, int aviableSheets){
+        if(requestedSheets>=aviableSheets){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public int stablishAmountOfSheets(int requestedSheets, int aviableSheets){
+        return aviableSheets-requestedSheets;
+    }
+
+    public int addSheets(int newSheets, int aviableSheets){
+        return newSheets+aviableSheets;
     }
 
 
