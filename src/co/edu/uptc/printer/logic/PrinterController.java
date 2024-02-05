82fdd8bb97efa2dc135f2a8b1ae373d39e0b7506
Nicolas <nicolas.garzon02@uptc.edu.co>
2024-02-05 18:43:41 -0500

    Added some methods

--- src/co/edu/uptc/printer/logic/PrinterController.java
+++ src/co/edu/uptc/printer/logic/PrinterController.java
@@ -2,6 +2,8 @@ package co.edu.uptc.printer.logic;
 
 import co.edu.uptc.printer.model.Printer;
 
+import java.util.ArrayList;
+
 public class PrinterController {
 
 
@@ -41,5 +43,40 @@ public class PrinterController {
         return "";
     }
 
+    public boolean checkMinSheets(int numSheets){
+        if(numSheets<1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean checkMaxSheets(int numSheets,int sheets){
+        if(numSheets>sheets){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
+    public ArrayList<Integer> refillSheets(int lenght){
+        ArrayList<Integer> filledPages= new ArrayList<>();
+        for (int i=0; i<filledPages.size();i++){
+            filledPages.add(i+1);
+        }
+        return filledPages;
+    }
+
+    public ArrayList<Integer> selectRangeSheets(ArrayList<Integer> sheets, int rangeSheetsMa, int rangeSheetsMi){
+        for (int i=0; i<rangeSheetsMi;i++){
+            if (sheets.get(i).equals(i+1)){
+                sheets.remove(i);
+            }
+        }
+        for (int i=rangeSheetsMa; i<sheets.size();i++){
+            if (sheets.get(i).equals(i+1)){
+                sheets.remove(i);
+            }
+        }
+        return sheets;
+    }
 }
